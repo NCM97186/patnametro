b7cec811c1368691a04f838f68d890bf3066e565
pushpendramca <pushpendra.kumar@netcreativemind.com>
2023-07-27 16:34:49 +0530

    laravel admin page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+     @include('../includes.head');
+   <body class="dashboard dashboard_1">
+      <div class="full_container">
+         <div class="inner_container">
+            <!-- Sidebar  -->
+             @include('../includes.sidebar');
+            </div>
+
+            <div id="content">
+               <!-- topbar -->
+               <div class="topbar">
+                 @include('../includes.header');
+               </div>
+               <!-- end topbar -->
+               <!-- dashboard inner -->
+              
+                     <section id="container" class=""> 
+                    @yield('content')
+                     </section>
+
+                  
+                @include('../includes.footer');
+   </body>
+</html>
